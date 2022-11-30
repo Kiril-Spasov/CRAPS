0157e11b16b338e07db9827e95122771fcd9187c
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-30 10:55:22 +0200

    Add project files.

---
+++ CRAPS/FormCraps.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CRAPS
+{
+    partial class FormCraps
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnPlay = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnPlay
+            // 
+            this.BtnPlay.Location = new System.Drawing.Point(47, 54);
+            this.BtnPlay.Name = "BtnPlay";
+            this.BtnPlay.Size = new System.Drawing.Size(186, 65);
+            this.BtnPlay.TabIndex = 0;
+            this.BtnPlay.Text = "Play";
+            this.BtnPlay.UseVisualStyleBackColor = true;
+            this.BtnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(287, 54);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(434, 357);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormCraps
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnPlay);
+            this.Name = "FormCraps";
+            this.Text = "CRAPS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnPlay;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ CRAPS/FormCraps.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CRAPS
+{
+    public partial class FormCraps : Form
+    {
+        public FormCraps()
+        {
+            InitializeComponent();
+        }
+
+        //Indicates how many throws had this game.
+        int throws = 0;
+        //Indicates when the player should have stopped.
+        int throwStop = 0;
+        
+        int thePoint = 0;
+        bool won;
+        bool gameOver;
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            int lineCount = 0;
+
+            string path = Application.StartupPath + @"\craps.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line = streamReader.ReadLine();
+
+                if (line == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    lineCount++;
+                    CheckGameResult(line);
+                    TxtResult.Text += "#" + lineCount + ": " + Display() + Environment.NewLine;
+                }
+            }
+        }
+
+        private void CheckGameResult(string input)
+        {
+            gameOver = false;
+            won = false;
+
+            string[] rolls = input.Split(' ');
+            throws = Convert.ToInt32(rolls[0]);
+            thePoint = Convert.ToInt32(rolls[1]);
+
+            //If the first throw was 7 or 11 the player won.
+            //If it was 2, 3 or 12 he lost.
+            if (Convert.ToInt32(rolls[1]) == 7 || Convert.ToInt32(rolls[1]) == 11)
+            {
+                won = true;
+                gameOver = true;
+                throwStop = 1;
+            }
+            else if (Convert.ToInt32(rolls[1]) == 2 || Convert.ToInt32(rolls[1]) == 3 || Convert.ToInt32(rolls[1]) == 12)
+            {
+                won = false;
+                gameOver = true;
+                throwStop = 1;
+            }
+            else
+            {
+                for (int i = 2; i < rolls.Length; i++)
+                {
+                    if (Convert.ToInt32(rolls[i]) == thePoint)
+                    {
+                        won = true;
+                        gameOver = true;
+                        throwStop = i;
+                    }
+                    else if (Convert.ToInt32(rolls[i]) == 7)
+                    {
+                        won = false;
+                        gameOver = true;
+                        throwStop = i;
+                    }
+                }
+            }   
+        }
+
+        private string Display()
+        {
+            string result = "";
+
+            if (won == true && gameOver == true)
+            {
+                result = throws == throwStop ? "Won!" : "Won, stop rolling!";
+            }
+            else if (won == false && gameOver == true)
+            {
+                result = throws == throwStop ? "Lost!" : "Lost, stop rolling!";
+            }
+            else if (gameOver == false)
+            {
+                result = "Keep rolling!";
+            }
+
+            return result;
+        }
+    }
+}
